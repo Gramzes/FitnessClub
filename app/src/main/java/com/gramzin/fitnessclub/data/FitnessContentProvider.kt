254720d9e4d1154bf3be1fc8438d9fbb925dfd15
Gramzes <gramzin4666@gmail.com>
2023-01-26 16:18:19 +0300

    ContentProvider added.

---
+++ app/src/main/java/com/gramzin/fitnessclub/data/FitnessContentProvider.kt
@@ -0,0 +1,42 @@
+package com.gramzin.fitnessclub.data
+
+import android.content.ContentProvider
+import android.content.ContentValues
+import android.database.Cursor
+import android.net.Uri
+
+class FitnessContentProvider: ContentProvider() {
+
+    lateinit var dbOpenHelper: FitnessDBHelper
+
+    override fun onCreate(): Boolean {
+        dbOpenHelper = FitnessDBHelper(context)
+        return true
+    }
+
+    override fun query(
+        p0: Uri,
+        p1: Array<out String>?,
+        p2: String?,
+        p3: Array<out String>?,
+        p4: String?
+    ): Cursor? {
+        TODO("Not yet implemented")
+    }
+
+    override fun insert(p0: Uri, p1: ContentValues?): Uri? {
+        TODO("Not yet implemented")
+    }
+
+    override fun delete(p0: Uri, p1: String?, p2: Array<out String>?): Int {
+        TODO("Not yet implemented")
+    }
+
+    override fun update(p0: Uri, p1: ContentValues?, p2: String?, p3: Array<out String>?): Int {
+        TODO("Not yet implemented")
+    }
+
+    override fun getType(p0: Uri): String? {
+        TODO("Not yet implemented")
+    }
+}
